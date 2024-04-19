b7a24ab811287ad51ea06e5f065076839be27a9a
Luiz Filipe <luiz.neves@prosas.com.br>
2024-04-19 15:05:08 -0300

    First version

---
+++ json_api_active_record_query_adapter.gemspec
@@ -0,0 +1,15 @@
+Gem::Specification.new do |s|
+  s.name        = "json_api_active_record_query_adapter"
+  s.version     = "1.0.0"
+  s.date        = "2023-04-19"
+  s.summary     = "Syntax query definition and adapter for using with active record."
+  s.description = "Syntax query definition and adapter for using with active record."
+  s.authors     = ["Lucas Hunter"]
+  s.email       = "ops@prosas.com.br"
+  s.files       = ["lib/json_api_active_record_query_adapter.rb"]
+  s.require_paths = ["lib"]
+  s.homepage    = "https://github.com/prosas/json_api_active_record_query_adapter"
+  s.license     = "MIT"
+
+  s.metadata['allowed_push_host'] = 'https://rubygems.org'
+end
---
+++ lib/json_api_active_record_query_adapter.rb
@@ -0,0 +1,139 @@
+# TODO: Extrair para uma gem
+# TODO: Resolver para o caso de ordenação por colunas de tabelas associadas
+# frozen_string_literal: true
+
+# Adpta objeto json para hash de consulta compativél com o active_record
+## Como usar
+# Inclua o module include JsonApiFilterAdapter na controller.
+# chame o método parse_filter_adapter(params)
+# ```
+# class Controller < ApplicationControllers
+#  Model.where(parse_filter_adapter(params))
+#	 ...
+# end
+# ```
+# TODO: Extrair para uma gem
+# TODO: Resolver para o caso de ordenação por colunas de tabelas associadas
+module JsonApiFilterAdapter
+  # Adpta objeto jsonvpara hash de consulta compativél com o active_record
+  # Ex:
+  # >> object_json_api = "{id: 1, data: 01/01/2022..31/01/2022}"
+  # >> adpter_from_json_api_to_active_record(object_json_api)
+  # >> {"id": 1, "data": Range(Date.parse("01/01/2022"), Date.parse("31/01/2022"))}
+  def parse_filter_adapter(data)
+    data = data_order_formater data, params
+
+    data_parsed = {}
+    data.each do |parameter|
+      # verifica se valor do filtro é um range
+      next unless parameter[VALUE].to_s.include?('..')
+
+      first, last = parameter[VALUE].split('..')
+      case parameter[VALUE]
+      when /^\d{4}-\d{2}-\d{2}..\d{4}-\d{2}-\d{2}$/
+        data_parsed[parameter[KEY]] = generate_date_range(first, last)
+      when /^\d+..\d+$/
+        data_parsed[parameter[KEY]] = Range.new(first.to_i, last.to_i)
+      when /^\d+.\d+..\d+.\d+$/
+        data_parsed[parameter[KEY]] = Range.new(first.to_d, last.to_d)
+      end
+    end
+
+    data.merge(data_parsed)
+
+    operator_parser_hash_to_array data
+  end
+
+  private
+
+  def data_order_formater(data, params)
+    data_modificated = {}
+    if params['filter'].present?
+      params['filter'].each do |parameter|
+        data_modificated[parameter[KEY]] = parameter[VALUE] if data[parameter[KEY]].present?
+      end
+    else
+      data_modificated = data
+    end
+    data_modificated
+  end
+
+  def generate_date_range(first, last)
+    # (first..last).select { |d| valid_date?(d) }
+    Range.new(Date.parse(first), Date.parse(last))
+  end
+
+  def operator_parser_hash_to_array(data)
+    data_converted_value = []
+    data_converted_header = ''
+    cont = 1
+
+    # interar o data para analisar isoladamente cada chave vs valor
+    data.each do |parameter|
+      # tratar operador de comparação
+      value_downcase = parameter[VALUE].to_s.downcase
+
+      if value_downcase.include?('=like=')
+        conditional_operator = 'LIKE ? '
+        parameter[VALUE] = "%#{parameter[VALUE]}%"
+      elsif parameter[VALUE].to_s.include?('..')
+        conditional_operator = 'BETWEEN ? AND ? '
+      elsif parameter[VALUE].is_a?(Array) && !value_downcase.include?('=null=')
+        conditional_operator = ' IN (?) '
+      elsif parameter[VALUE].is_a?(Array) && value_downcase.include?('=null=')
+        parameter[VALUE] = parameter[VALUE].filter { |v| v != '=null=' }
+        conditional_operator = " IN (?) OR #{parameter[KEY]} IS NULL"
+      elsif !parameter[VALUE].is_a?(Array) && value_downcase.include?('=null=')
+        parameter[VALUE] = nil
+        conditional_operator = ' IS NULL'
+      else
+        conditional_operator = '= ? '
+      end
+
+      # tratar operador de pesquisa
+      query_conditional_operator = value_downcase.include?('=or=') ? ' OR' : ' AND'
+      # criar a 1º string do vetor que carrega o corpo da pesquisa
+      # saber se esta no final da lista de parametros
+      if cont < data.to_hash.size
+        # se não tiver no final da lista
+        data_converted_header = data_converted_header + parameter[KEY] + " #{conditional_operator} #{query_conditional_operator} "
+        cont += 1
+      else
+        # se tiver no final da lista
+        data_converted_header = data_converted_header + parameter[KEY] + " #{conditional_operator} "
+        cont += 1
+      end
+    end
+
+    # criar os demais elementos do vetor que faram referencia em ordem a cada um dos pontos de interrogação
+    data.to_hash.each_value do |value|
+      # tratando entradas que não são arrays
+      unless value.is_a?(Array)
+
+        value_modificad = value.to_s.downcase
+        # tratar like e or
+        if value_modificad.include?('=like=')
+          value_modificad.slice! '=like='
+          value_modificad = "%#{value_modificad}%"
+        end
+        value_modificad.slice! '=or=' if value_modificad.include?('=or=')
+        if value_modificad.include?('true') || value_modificad.include?('false')
+          data_converted_value << value_modificad.to_bool
+        elsif !value_modificad.include?('=null=')
+          # tratar between
+          if value_modificad.include?('..')
+            first, last = value_modificad.split('..')
+            data_converted_value << first
+            data_converted_value << last
+          end
+          data_converted_value << value_modificad unless value_modificad.include?('..')
+        end
+      end
+      # tratando entradas que são array
+      data_converted_value << value.filter { |v| v != '=null=' } if value.is_a?(Array)
+    end
+    # montar array de busca , primeira posição string query , segunda posição ate N serão parametros
+    data_converted_value.unshift(data_converted_header)
+    data_converted_value
+  end
+end
