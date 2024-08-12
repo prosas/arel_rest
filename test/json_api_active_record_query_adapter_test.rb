d1ee7b8ce35a0ac31a2cebca0f9ebc03b797b72b
Luiz Filipe <luiz.neves@prosas.com.br>
2024-08-12 17:01:04 -0300

    Fix filtro null

--- test/json_api_active_record_query_adapter_test.rb
+++ test/json_api_active_record_query_adapter_test.rb
@@ -11,8 +11,10 @@ class JsonApiFilterAdapterTest < Minitest::Test
 	end
 
 	def test_parse_filter_adapter
-		assert_equal @_class.parse_filter_adapter({"row.colum1" => 20, "row.colum2" => ["=null="]}),["row.colum1 = ? AND row.colum2 IN (?) ", "20", [nil]]
+		assert_equal @_class.parse_filter_adapter({"row.colum1" => 20, "row.colum2" => ["=null=", "teste"]}),["row.colum1 = ? AND (row.colum2 IS NULL OR row.colum2 IN (?))", "20", ["teste"]]
 		
-		assert_equal @_class.parse_filter_adapter({"row.colum1" => 20, "row.colum2" => "=null="}),["row.colum1 = ? AND row.colum2 IS NULL ", "20"]
+		assert_equal @_class.parse_filter_adapter({"row.colum1" => 20, "row.colum2" => "=null="}),["row.colum1 = ? AND row.colum2 IS NULL", "20"]
+		
+		assert_equal @_class.parse_filter_adapter({"row.colum1" => 20, "row.colum2" => "=like=teste"}),["row.colum1 = ? AND row.colum2 LIKE ?", "20", "%teste%"]
 	end
 end
