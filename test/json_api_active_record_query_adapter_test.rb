590e16254f4c87cb286fc259499fa9745f69c19e
Rafael Abreu <web.nogs@gmail.com>
2024-09-05 17:13:27 -0300

    [fix]: adjust module to support date when it is type range

--- test/json_api_active_record_query_adapter_test.rb
+++ test/json_api_active_record_query_adapter_test.rb
@@ -49,12 +49,16 @@ class JsonApiFilterAdapterTest < Minitest::Test
   end
 
   def test_range_with_dates
-    assert_equal @_class.parse_filter_adapter({"row.colum1" => "2024-01-01..2024-01-31"}),
-    ["row.colum1 BETWEEN ? AND ?", "2024-01-01", "2024-01-31"]
+    result = @_class.parse_filter_adapter({ "row.colum1" => "2024-09-05..2024-09-30" })
+    expected = ["row.colum1 BETWEEN ? AND ?", Date.new(2024, 9, 5), Date.new(2024, 9, 30)]
+    
+    assert_equal expected, result
   end
   
   def test_range_with_dates_and_times
-    assert_equal @_class.parse_filter_adapter({"row.colum1" => "2024-01-01 14:10..2024-01-01 16:00"}),
-      ["row.colum1 BETWEEN ? AND ?", "2024-01-01 14:10", "2024-01-01 16:00"]
+    result = @_class.parse_filter_adapter({ "row.colum1" => "2024-09-05 00:00..2024-09-05 23:59" })
+    expected = ["row.colum1 BETWEEN ? AND ?", Time.new(2024, 9, 5, 0, 0, 0), Time.new(2024, 9, 5, 23, 59, 0)]
+    
+    assert_equal expected, result
   end
 end
